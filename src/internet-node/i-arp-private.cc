a6a747dedd2ee7cb3c2209dd3e2dd74e1cd5216d
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2007-06-08 18:58:34 +0100

    Add my institutional email address (gjc@inescporto.pt), in addition to the GMail one.

---
+++ src/internet-node/i-arp-private.cc
@@ -0,0 +1,55 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2007 INRIA
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#include "i-arp-private.h"
+#include "arp.h"
+#include "ns3/assert.h"
+#include "ns3/net-device.h"
+
+namespace ns3 {
+
+const InterfaceId IArpPrivate::iid ("IArpPrivate");
+
+IArpPrivate::IArpPrivate (Ptr<Arp> arp)
+  : Interface (IArpPrivate::iid),
+    m_arp (arp)
+{}
+IArpPrivate::~IArpPrivate ()
+{
+  NS_ASSERT (m_arp == 0);
+}
+
+bool 
+IArpPrivate::Lookup (Packet &p, Ipv4Address destination, 
+		     Ptr<NetDevice> device,
+		     MacAddress *hardwareDestination)
+{
+  return m_arp->Lookup (p, destination, device, hardwareDestination);
+}
+
+void
+IArpPrivate::DoDispose (void)
+{
+  m_arp = 0;
+  Interface::DoDispose ();
+}
+
+
+} // namespace ns3
