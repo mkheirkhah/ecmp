7f5ae0363c7552b09d4a76d2bf77089dfc6a7a96
Tom Henderson <tomh@tomh.org>
2008-03-21 12:14:07 -0700

    tutorial updates for attributes

---
+++ src/core/log.cc
@@ -0,0 +1,365 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2006,2007 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+
+// What about print-list!!!!!!???????
+
+#ifdef NS3_LOG_ENABLE
+
+#include <list>
+#include <utility>
+#include <iostream>
+#include "log.h"
+#include "assert.h"
+#include "ns3/core-config.h"
+#include "fatal-error.h"
+
+#ifdef HAVE_STDLIB_H
+#include <stdlib.h>
+#endif
+
+namespace ns3 {
+
+typedef std::list<std::pair <std::string, LogComponent *> > ComponentList;
+typedef std::list<std::pair <std::string, LogComponent *> >::iterator ComponentListI;
+
+static class PrintList
+{
+public:
+  PrintList ();
+} g_printList;
+
+static 
+ComponentList *GetComponentList (void)
+{
+  static ComponentList components;
+  return &components;
+}
+
+
+
+PrintList::PrintList ()
+{
+#ifdef HAVE_GETENV
+  char *envVar = getenv("NS_LOG");
+  if (envVar == 0)
+    {
+      return;
+    }
+  std::string env = envVar;
+  std::string::size_type cur = 0;
+  std::string::size_type next = 0;
+  while (next != std::string::npos)
+    {
+      next = env.find_first_of (";", cur);
+      std::string tmp = std::string (env, cur, next-cur);
+      if (tmp == "print-list")
+        {
+          LogComponentPrintList ();
+          exit (0);
+          break;
+        }
+      cur = next + 1;
+    }
+#endif  
+}
+
+
+LogComponent::LogComponent (char const * name)
+  : m_levels (0), m_name (name)
+{
+  EnvVarCheck (name);
+
+  ComponentList *components = GetComponentList ();
+  for (ComponentListI i = components->begin ();
+       i != components->end ();
+       i++)
+    {
+      NS_ASSERT (i->first != name);
+    }
+  components->push_back (std::make_pair (name, this));
+}
+
+void
+LogComponent::EnvVarCheck (char const * name)
+{
+#ifdef HAVE_GETENV
+  char *envVar = getenv("NS_LOG");
+  if (envVar == 0)
+    {
+      return;
+    }
+  std::string env = envVar;
+  std::string myName = name;
+
+  std::string::size_type cur = 0;
+  std::string::size_type next = 0;
+  while (next != std::string::npos)
+    {
+      next = env.find_first_of (";", cur);
+      std::string tmp = std::string (env, cur, next-cur);
+      std::string::size_type equal = tmp.find ("=");
+      std::string component;
+      if (equal == std::string::npos)
+        {
+          component = tmp;
+          if (component == myName || component == "*")
+            {
+              Enable (LOG_DEBUG);
+              return;
+            }
+        }
+      else
+        {
+          component = tmp.substr (0, equal);
+          if (component == myName || component == "*")
+            {
+              int level = 0;
+              std::string::size_type cur_lev;
+              std::string::size_type next_lev = equal;
+              do
+                {
+                  cur_lev = next_lev + 1;
+                  next_lev = tmp.find ("|", cur_lev);
+                  std::string lev = tmp.substr (cur_lev, next_lev - cur_lev);
+                  if (lev == "error")
+                    {
+                      level |= LOG_ERROR;
+                    }
+                  else if (lev == "warn")
+                    {
+                      level |= LOG_WARN;
+                    }
+                  else if (lev == "debug")
+                    {
+                      level |= LOG_DEBUG;
+                    }
+                  else if (lev == "info")
+                    {
+                      level |= LOG_INFO;
+                    }
+                  else if (lev == "function")
+                    {
+                      level |= LOG_FUNCTION;
+                    }
+                  else if (lev == "param")
+                    {
+                      level |= LOG_PARAM;
+                    }
+                  else if (lev == "logic")
+                    {
+                      level |= LOG_LOGIC;
+                    }
+                  else if (lev == "all")
+                    {
+                      level |= LOG_ALL;
+                    }
+                  else if (lev == "prefix")
+                    {
+                      level |= LOG_PREFIX_ALL;
+                    }
+                  else if (lev == "level_error")
+                    {
+                      level |= LOG_LEVEL_ERROR;
+                    }
+                  else if (lev == "level_warn")
+                    {
+                      level |= LOG_LEVEL_WARN;
+                    }
+                  else if (lev == "level_debug")
+                    {
+                      level |= LOG_LEVEL_DEBUG;
+                    }
+                  else if (lev == "level_info")
+                    {
+                      level |= LOG_LEVEL_INFO;
+                    }
+                  else if (lev == "level_function")
+                    {
+                      level |= LOG_LEVEL_FUNCTION;
+                    }
+                  else if (lev == "level_param")
+                    {
+                      level |= LOG_LEVEL_PARAM;
+                    }
+                  else if (lev == "level_logic")
+                    {
+                      level |= LOG_LEVEL_LOGIC;
+                    }
+                  else if (lev == "level_all")
+                    {
+                      level |= LOG_LEVEL_ALL;
+                    }
+                } while (next_lev != std::string::npos);
+
+              Enable ((enum LogLevel)level);
+            }
+        }
+      cur = next + 1;
+    }
+#endif
+}
+
+
+bool 
+LogComponent::IsEnabled (enum LogLevel level) const
+{
+  //  LogComponentEnableEnvVar ();
+  return (level & m_levels) ? 1 : 0;
+}
+
+bool
+LogComponent::IsNoneEnabled (void) const
+{
+  return m_levels == 0;
+}
+
+void 
+LogComponent::Enable (enum LogLevel level)
+{
+  m_levels |= level;
+}
+
+void 
+LogComponent::Disable (enum LogLevel level)
+{
+  m_levels &= ~level;
+}
+
+char const *
+LogComponent::Name (void) const
+{
+  return m_name;
+}
+
+
+void 
+LogComponentEnable (char const *name, enum LogLevel level)
+{
+  ComponentList *components = GetComponentList ();
+  for (ComponentListI i = components->begin ();
+       i != components->end ();
+       i++)
+    {
+      if (i->first.compare (name) == 0) 
+	{
+	  i->second->Enable (level);
+	  break;
+	}
+    }  
+}
+
+void 
+LogComponentEnableAll (enum LogLevel level)
+{
+  ComponentList *components = GetComponentList ();
+  for (ComponentListI i = components->begin ();
+       i != components->end ();
+       i++)
+    {
+      i->second->Enable (level);
+    }  
+}
+
+void 
+LogComponentDisable (char const *name, enum LogLevel level)
+{
+  ComponentList *components = GetComponentList ();
+  for (ComponentListI i = components->begin ();
+       i != components->end ();
+       i++)
+    {
+      if (i->first.compare (name) == 0) 
+	{
+	  i->second->Disable (level);
+	  break;
+	}
+    }  
+}
+
+void 
+LogComponentDisableAll (enum LogLevel level)
+{
+  ComponentList *components = GetComponentList ();
+  for (ComponentListI i = components->begin ();
+       i != components->end ();
+       i++)
+    {
+      i->second->Disable (level);
+    }  
+}
+
+void 
+LogComponentPrintList (void)
+{
+  ComponentList *components = GetComponentList ();
+  for (ComponentListI i = components->begin ();
+       i != components->end ();
+       i++)
+    {
+      std::cout << i->first << "=";
+      if (i->second->IsNoneEnabled ())
+        {
+          std::cout << "0" << std::endl;
+          continue;
+        }
+      if (i->second->IsEnabled (LOG_ERROR))
+        {
+          std::cout << "error";
+        }
+      if (i->second->IsEnabled (LOG_WARN))
+        {
+          std::cout << "|warn";
+        }
+      if (i->second->IsEnabled (LOG_DEBUG))
+        {
+          std::cout << "|debug";
+        }
+      if (i->second->IsEnabled (LOG_INFO))
+        {
+          std::cout << "|info";
+        }
+      if (i->second->IsEnabled (LOG_FUNCTION))
+        {
+          std::cout << "|function";
+        }
+      if (i->second->IsEnabled (LOG_PARAM))
+        {
+          std::cout << "|param";
+        }
+      if (i->second->IsEnabled (LOG_LOGIC))
+        {
+          std::cout << "|logic";
+        }
+      if (i->second->IsEnabled (LOG_ALL))
+        {
+          std::cout << "|all";
+        }
+      std::cout << std::endl;
+    }
+}
+
+
+ParameterLogger g_parameterLogger;
+EndParameterListStruct EndParameterList;
+
+
+} // namespace ns3
+
+#endif // NS3_LOG_ENABLE
---
+++ src/helper/mobility-helper.cc
@@ -0,0 +1,148 @@
+#include "ns3/mobility-helper.h"
+#include "ns3/mobility-model.h"
+#include "ns3/mobility-model-notifier.h"
+#include "ns3/position-allocator.h"
+#include "ns3/hierarchical-mobility-model.h"
+#include "ns3/log.h"
+
+namespace ns3 {
+
+NS_LOG_COMPONENT_DEFINE ("MobilityHelper");
+
+MobilityHelper::MobilityHelper ()
+  : m_notifierEnabled (false)
+{
+  m_position = CreateObject<RandomRectanglePositionAllocator> ("X", ConstantVariable (0.0),
+								   "Y", ConstantVariable (0.0));
+  m_mobility.SetTypeId ("ns3::StaticMobilityModel");
+}
+void 
+MobilityHelper::EnableNotifier (void)
+{
+  m_notifierEnabled = true;
+}
+void 
+MobilityHelper::DisableNotifier (void)
+{
+  m_notifierEnabled = false;
+}
+void 
+MobilityHelper::SetPositionAllocator (Ptr<PositionAllocator> allocator)
+{
+  m_position = allocator;
+}
+void 
+MobilityHelper::SetPositionAllocator (std::string type,
+				      std::string n1, Attribute v1,
+				      std::string n2, Attribute v2,
+				      std::string n3, Attribute v3,
+				      std::string n4, Attribute v4,
+				      std::string n5, Attribute v5,
+				      std::string n6, Attribute v6,
+				      std::string n7, Attribute v7,
+				      std::string n8, Attribute v8,
+				      std::string n9, Attribute v9)
+{
+  ObjectFactory pos;
+  pos.SetTypeId (type);
+  pos.Set (n1, v1);
+  pos.Set (n2, v2);
+  pos.Set (n3, v3);
+  pos.Set (n4, v4);
+  pos.Set (n5, v5);
+  pos.Set (n6, v6);
+  pos.Set (n7, v7);
+  pos.Set (n8, v8);
+  pos.Set (n9, v9);
+  m_position = pos.Create ()->GetObject<PositionAllocator> ();
+}
+
+void 
+MobilityHelper::SetMobilityModel (std::string type,
+				  std::string n1, Attribute v1,
+				  std::string n2, Attribute v2,
+				  std::string n3, Attribute v3,
+				  std::string n4, Attribute v4,
+				  std::string n5, Attribute v5,
+				  std::string n6, Attribute v6,
+				  std::string n7, Attribute v7,
+				  std::string n8, Attribute v8,
+				  std::string n9, Attribute v9)
+{
+  m_mobility.SetTypeId (type);
+  m_mobility.Set (n1, v1);
+  m_mobility.Set (n2, v2);
+  m_mobility.Set (n3, v3);
+  m_mobility.Set (n4, v4);
+  m_mobility.Set (n5, v5);
+  m_mobility.Set (n6, v6);
+  m_mobility.Set (n7, v7);
+  m_mobility.Set (n8, v8);
+  m_mobility.Set (n9, v9);
+}
+
+void 
+MobilityHelper::PushReferenceMobilityModel (Ptr<Object> reference)
+{
+  Ptr<MobilityModel> mobility = reference->GetObject<MobilityModel> ();
+  m_mobilityStack.push_back (mobility);
+}
+void 
+MobilityHelper::PopReferenceMobilityModel (void)
+{
+  m_mobilityStack.pop_back ();
+}
+
+
+std::string 
+MobilityHelper::GetMobilityModelType (void) const
+{
+  return m_mobility.GetTypeId ().GetName ();
+}
+
+void 
+MobilityHelper::Layout (const std::vector<Ptr<Object> > &objects)
+{
+  for (std::vector<Ptr<Object> >::const_iterator i = objects.begin (); i != objects.end (); i++)
+    {
+      Ptr<Object> object = *i;
+      Ptr<MobilityModel> model = object->GetObject<MobilityModel> ();
+      if (model == 0)
+	{
+	  model = m_mobility.Create ()->GetObject<MobilityModel> ();
+	  if (model == 0)
+	    {
+	      NS_FATAL_ERROR ("The requested mobility model is not a mobility model: \""<< 
+			      m_mobility.GetTypeId ().GetName ()<<"\"");
+	    }
+	  if (m_mobilityStack.empty ())
+	    {
+	      NS_LOG_DEBUG ("node="<<object<<", mob="<<model);
+	      object->AggregateObject (model);
+	    }
+	  else
+	    {
+	      // we need to setup a hierarchical mobility model
+	      Ptr<MobilityModel> parent = m_mobilityStack.back ();
+	      Ptr<MobilityModel> hierarchical = 
+		CreateObject<HierarchicalMobilityModel> ("Child", model,
+							 "Parent", parent);
+	      object->AggregateObject (hierarchical);
+	      NS_LOG_DEBUG ("node="<<object<<", mob="<<hierarchical);
+	    }
+	}
+      Vector position = m_position->GetNext ();
+      model->SetPosition (position);
+      if (m_notifierEnabled)
+	{
+	  Ptr<MobilityModelNotifier> notifier = 
+	    object->GetObject<MobilityModelNotifier> ();
+	  if (notifier == 0)
+	    {
+	      object->AggregateObject (CreateObject<MobilityModelNotifier> ());
+	    }
+	}
+    }
+}
+
+} // namespace ns3
