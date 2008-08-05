dba93d4bc568541c24388ef9561424cb888ad468
Gustavo J. A. M. Carneiro <gjc@inescporto.pt>
2008-08-05 19:42:53 +0100

    Python: re-scan API; unblock python threads during Simulator::Run; new pybindgen brings improved thread safety for virtual methods.

---
+++ src/helper/on-off-helper.h
@@ -0,0 +1,52 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2008 INRIA
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Mathieu Lacage <mathieu.lacage@sophia.inria.fr>
+ */
+#ifndef ON_OFF_HELPER_H
+#define ON_OFF_HELPER_H
+
+#include <stdint.h>
+#include <string>
+#include "ns3/object-factory.h"
+#include "ns3/address.h"
+#include "ns3/attribute.h"
+#include "ns3/net-device.h"
+#include "node-container.h"
+#include "application-container.h"
+
+namespace ns3 {
+
+class OnOffHelper
+{
+public:
+  OnOffHelper (std::string protocol, Address address);
+
+  void SetAttribute (std::string name, const AttributeValue &value);
+
+  ApplicationContainer Install (NodeContainer c);
+
+private:
+  std::string m_protocol;
+  Address m_remote;
+  ObjectFactory m_factory;
+};
+
+} // namespace ns3
+
+#endif /* ON_OFF_HELPER_H */
+
