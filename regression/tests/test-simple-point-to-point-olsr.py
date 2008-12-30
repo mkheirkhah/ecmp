c713be8b072ff4e98eb33de57aba76bfa3d7b5f5
Tom Henderson <tomh@tomh.org>
2008-12-30 11:35:31 -0800

    fix for bug451-- arp cache

---
+++ regression/tests/test-simple-point-to-point-olsr.py
@@ -0,0 +1,13 @@
+#! /usr/bin/env python
+
+"""Generic trace-comparison-type regression test."""
+
+import os
+import shutil
+import tracediff
+
+def run(verbose, generate):
+    """Execute a test."""
+
+    return tracediff.run_test(verbose, generate,
+        "simple-point-to-point-olsr")
