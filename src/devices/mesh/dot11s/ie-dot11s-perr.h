a8fc5953b193bbfe9a1a703a7cf69b2367bfb5a5
Pavel Boyko <boyko@iitp.ru>
2009-04-01 16:14:05 +0400

    Make routing protocol attribute of mesh point to be accessable by Config::

---
+++ src/devices/mesh/dot11s/ie-dot11s-perr.h
@@ -0,0 +1,66 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2008,2009 IITP RAS
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Kirill Andreev <andreev@iitp.ru>
+ */
+
+
+#ifndef PERR_INFORMATION_ELEMENT_H
+#define PERR_INFORMATION_ELEMENT_H
+
+#include "ns3/mac48-address.h"
+#include "ns3/wifi-information-element.h"
+
+namespace ns3 {
+namespace dot11s {
+/**
+ * \ingroup dot11s
+ * \brief See 7.3.2.98 of 802.11s draft 2.07
+ */
+class IePerr : public WifiInformationElement
+{
+public:
+  IePerr ();
+  ~IePerr ();
+  static  TypeId   GetTypeId ();
+  virtual TypeId   GetInstanceTypeId () const;
+  struct FailedDestination
+  {
+    Mac48Address destination;
+    uint32_t seqnum;
+  };
+  uint8_t   GetNumOfDest ();
+
+  void   AddAddressUnit (struct FailedDestination unit);
+  std::vector<FailedDestination>  GetAddressUnitVector ();
+  void   DeleteAddressUnit (Mac48Address address);
+  void   ResetPerr ();
+private:
+  WifiElementId ElementId () const{
+    return IE11S_PERR;
+  };
+  void  SerializeInformation (Buffer::Iterator i) const;
+  uint8_t  DeserializeInformation (Buffer::Iterator start, uint8_t length);
+  void PrintInformation (std::ostream& os) const;
+  uint8_t  GetInformationSize () const;
+  uint8_t   m_numOfDest;
+  std::vector<FailedDestination> m_addressUnits;
+};
+  
+} // namespace dot11s
+} //namespace ns3
+#endif
