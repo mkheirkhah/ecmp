a2a0fd5d8f43907c8e29980cdfca23e2f1918c2d
Tom Henderson <tomh@tomh.org>
2009-05-29 13:33:20 -0700

    add comment about NS_OBJECT_ENSURE_REGISTERED() macro to the manual

---
+++ src/applications/udp-echo/udp-echo-client.h
@@ -0,0 +1,77 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright 2007 University of Washington
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#ifndef __UDP_ECHO_CLIENT_H__
+#define __UDP_ECHO_CLIENT_H__
+
+#include "ns3/application.h"
+#include "ns3/event-id.h"
+#include "ns3/ptr.h"
+#include "ns3/ipv4-address.h"
+
+namespace ns3 {
+
+class Socket;
+class Packet;
+
+/**
+ * \ingroup udpecho
+ * \brief A Udp Echo client
+ *
+ * Every packet sent should be returned by the server and received here.
+ */
+class UdpEchoClient : public Application 
+{
+public:
+  static TypeId GetTypeId (void);
+
+  UdpEchoClient ();
+
+  virtual ~UdpEchoClient ();
+
+  void SetRemote (Ipv4Address ip, uint16_t port);
+
+protected:
+  virtual void DoDispose (void);
+
+private:
+
+  virtual void StartApplication (void);
+  virtual void StopApplication (void);
+
+  void ScheduleTransmit (Time dt);
+  void Send (void);
+
+  void HandleRead (Ptr<Socket> socket);
+
+  uint32_t m_count;
+  Time m_interval;
+  uint32_t m_size;
+
+  uint32_t m_sent;
+  Ptr<Socket> m_socket;
+  Ipv4Address m_peerAddress;
+  uint16_t m_peerPort;
+  EventId m_sendEvent;
+
+};
+
+} // namespace ns3
+
+#endif // __UDP_ECHO_CLIENT_H__
+
