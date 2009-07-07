93f5b67bd010d916c93d6b299e2ae6516f658e67
Pavel Boyko <boyko@iitp.ru>
2009-07-07 13:41:03 +0400

    packets cosmetics

---
+++ src/routing/aodv/aodv-packet.cc
@@ -0,0 +1,586 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 1997, 1998 Carnegie Mellon University.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: The AODV code developed by the CMU/MONARCH group was optimized and
+ * tuned by Samir Das and Mahesh Marina, University of Cincinnati.
+ * The work was partially done in Sun Microsystems.
+ *
+ * Ported to ns-3 by Elena Borovkova <borovkovaes@iitp.ru>
+ */
+#include "ns3/test.h"
+#include "aodv-packet.h"
+#include "ns3/address-utils.h"
+#include "ns3/packet.h"
+
+namespace ns3 {
+namespace aodv {
+
+//-----------------------------------------------------------------------------
+// RREQ
+//-----------------------------------------------------------------------------
+RreqHeader::RreqHeader () : rq_flags(0), reserved(0), rq_hop_count(0), rq_bcast_id(0), rq_dst_seqno(0), rq_src_seqno(0)
+{
+  // TODO check defaults in AODV UU
+  SetGratiousRrep (false);
+  SetDestinationOnly (false);
+  SetUnknownSeqno (false);
+}
+
+TypeId RreqHeader::GetInstanceTypeId() const
+{
+  return TypeId();
+}
+
+uint32_t 
+RreqHeader::GetSerializedSize () const
+{
+  return 24;
+}
+
+void 
+RreqHeader::Serialize (Buffer::Iterator i) const
+{
+  i.WriteU8 (type());
+  i.WriteU8 (rq_flags);
+  i.WriteU8 (reserved);
+  i.WriteU8 (rq_hop_count);
+  i.WriteHtonU32 (rq_bcast_id);
+  WriteTo (i, rq_dst);
+  i.WriteHtonU32 (rq_dst_seqno);
+  WriteTo (i, rq_src);
+  i.WriteHtonU32 (rq_src_seqno);
+}
+
+uint32_t 
+RreqHeader::Deserialize (Buffer::Iterator start)
+{
+  Buffer::Iterator i = start;
+  uint8_t t = i.ReadU8 ();
+  NS_ASSERT (t == type());
+
+  rq_flags = i.ReadU8 ();
+  reserved = i.ReadU8 ();
+  rq_hop_count = i.ReadU8 ();
+  rq_bcast_id = i.ReadNtohU32 ();
+  ReadFrom (i, rq_dst);
+  rq_dst_seqno = i.ReadNtohU32 ();
+  ReadFrom (i, rq_src);
+  rq_src_seqno = i.ReadNtohU32 ();
+
+  uint32_t dist = i.GetDistanceFrom (start);
+  NS_ASSERT (dist == GetSerializedSize ());
+  return dist;
+}
+
+void 
+RreqHeader::Print (std::ostream &os) const
+{
+  os << "RREQ ID " << rq_bcast_id << "\n"
+  << "destination: ipv4 " << rq_dst << " "
+  << "sequence number " << rq_dst_seqno << "\n"
+  << "source: ipv4 " << rq_src << " "
+  << "sequence number " << rq_src_seqno << "\n"
+  << "flags:\n"
+  << "Gratuitous RREP " << (*this).GetGratiousRrep() << "\n"
+  << "Destination only " << (*this).GetDestinationOnly() << "\n"
+  << "Unknown sequence number " << (*this).GetUnknownSeqno() << "\n";
+}
+
+std::ostream & operator<<(std::ostream & os, RreqHeader const & h)
+{
+  h.Print (os);
+  return os;
+}
+
+void 
+RreqHeader::SetGratiousRrep (bool f)
+{
+  if (f) rq_flags |= (1 << 2);
+  else   rq_flags &= ~(1 << 2);
+}
+
+bool 
+RreqHeader::GetGratiousRrep () const
+{
+  return (rq_flags & (1 << 2));
+}
+
+void 
+RreqHeader::SetDestinationOnly (bool f)
+{
+  if (f) rq_flags |= (1 << 3);
+  else   rq_flags &= ~(1 << 3);
+}
+
+bool 
+RreqHeader::GetDestinationOnly () const
+{
+  return (rq_flags & (1 << 3));
+}
+
+void 
+RreqHeader::SetUnknownSeqno (bool f)
+{
+  if (f) rq_flags |= (1 << 4);
+  else   rq_flags &= ~(1 << 4);
+}
+
+bool 
+RreqHeader::GetUnknownSeqno () const
+{
+  return (rq_flags & (1 << 4));
+}
+
+bool
+RreqHeader::operator==(RreqHeader const & o) const
+{
+  return (rq_flags == o.rq_flags && reserved == o.reserved &&
+      rq_hop_count == o.rq_hop_count && rq_bcast_id == o.rq_bcast_id &&
+      rq_dst == o.rq_dst && rq_dst_seqno == o.rq_dst_seqno && 
+      rq_src == o.rq_src && rq_src_seqno == o.rq_src_seqno);
+}
+
+#ifdef RUN_SELF_TESTS
+/// Unit test for RREQ
+struct RreqHeaderTest : public Test
+{
+  RreqHeaderTest () : Test ("AODV/RREQ") {}
+  virtual bool RunTests();
+};
+
+/// Test instance
+static RreqHeaderTest g_RreqHeaderTest;
+
+bool RreqHeaderTest::RunTests ()
+{
+  bool result(true);
+
+  RreqHeader h;
+  h.SetDst (Ipv4Address("1.2.3.4"));
+  h.SetDstSeqno (123);
+  h.SetSrc (Ipv4Address("4.3.2.1"));
+  h.SetSrcSeqno (321);
+  h.SetId (1);
+  h.SetGratiousRrep (true);
+  NS_TEST_ASSERT(h.GetGratiousRrep ());
+  h.SetDestinationOnly (true);
+  NS_TEST_ASSERT(h.GetDestinationOnly ());
+  h.SetUnknownSeqno (true);
+  NS_TEST_ASSERT(h.GetUnknownSeqno ());
+  Ptr<Packet> p = Create<Packet> ();
+  p->AddHeader (h);
+  RreqHeader h2;
+  uint32_t bytes = p->RemoveHeader(h2);
+  NS_TEST_ASSERT_EQUAL (bytes, 24);
+  NS_TEST_ASSERT_EQUAL (h, h2);
+  return result;
+}
+#endif
+
+//-----------------------------------------------------------------------------
+// RREP
+//-----------------------------------------------------------------------------
+
+RrepHeader::RrepHeader() :rp_flags(0), prefixSize(0),  rp_hop_count(0), rp_dst_seqno(0)
+{
+  SetAckRequired(false);
+}
+
+TypeId
+RrepHeader::GetInstanceTypeId() const
+{
+  return TypeId();
+}
+
+uint32_t
+RrepHeader::GetSerializedSize () const
+{
+  return 20;
+}
+
+void
+RrepHeader::Serialize (Buffer::Iterator i) const
+{
+  i.WriteU8(type());
+  i.WriteU8(rp_flags);
+  i.WriteU8(prefixSize);
+  i.WriteU8 (rp_hop_count);
+  WriteTo (i, rp_dst);
+  i.WriteHtonU32 (rp_dst_seqno);
+  WriteTo (i, rp_src);
+  i.WriteHtonU32 (rp_lifetime);
+}
+
+uint32_t
+RrepHeader::Deserialize (Buffer::Iterator start)
+{
+  Buffer::Iterator i = start;
+  uint8_t t = i.ReadU8 ();
+  NS_ASSERT (t == type());
+
+  rp_flags = i.ReadU8 ();
+  prefixSize = i.ReadU8 ();
+  rp_hop_count = i.ReadU8 ();
+  ReadFrom (i, rp_dst);
+  rp_dst_seqno = i.ReadNtohU32 ();
+  ReadFrom (i, rp_src);
+  rp_lifetime = i.ReadNtohU32 ();
+
+  uint32_t dist = i.GetDistanceFrom (start);
+  NS_ASSERT (dist == GetSerializedSize ());
+  return dist;
+}
+
+void
+RrepHeader::Print (std::ostream &os) const
+{
+  os << "RREP\n"
+  << "destination: ipv4 " << rp_dst
+  << "sequence number " <<  rp_dst_seqno;
+  if(prefixSize != 0)
+    os << "prefix size " << prefixSize << "\n";
+  else os << "\n";
+  os << "source ipv4 " << rp_src
+  << "life time " << rp_lifetime << "\n"
+  << "acknowledgment required flag " << (*this).GetAckRequired() << "\n";
+}
+
+void
+RrepHeader::SetAckRequired (bool f)
+{
+  if (f) rp_flags |= (1 << 1);
+  else   rp_flags &= ~(1 << 1);
+}
+
+bool
+RrepHeader::GetAckRequired () const
+{
+  return (rp_flags & (1 << 1));
+}
+
+void
+RrepHeader::SetPrefixSize(uint8_t sz)
+{
+  prefixSize = sz;
+}
+
+uint8_t
+RrepHeader::GetPrefixSize() const
+{
+  return prefixSize;
+}
+
+bool
+RrepHeader::operator==(RrepHeader const & o) const
+{
+  return (rp_flags == o.rp_flags && prefixSize == o.prefixSize &&
+      rp_hop_count == o.rp_hop_count && rp_dst == o.rp_dst &&
+      rp_dst_seqno == o.rp_dst_seqno && rp_src == o.rp_src &&
+      rp_lifetime == o.rp_lifetime);
+}
+
+
+void
+RrepHeader::SetHello(Ipv4Address src, uint32_t srcSeqNo)
+{
+  rp_flags = 0;
+  prefixSize = 0;
+  rp_hop_count = 0;
+  rp_dst = src;
+  rp_dst_seqno = srcSeqNo;
+  rp_src = src;
+  rp_lifetime = HELLO_INTERVAL * ALLOWED_HELLO_LOSS;
+}
+
+std::ostream & operator<<(std::ostream & os, RrepHeader const & h)
+{
+  h.Print (os);
+  return os;
+}
+
+#ifdef RUN_SELF_TESTS
+/// Unit test for RREP
+struct RrepHeaderTest : public Test
+{
+  RrepHeaderTest () : Test ("AODV/RREP") {}
+  virtual bool RunTests();
+};
+
+/// Test instance
+static RrepHeaderTest g_RrepHeaderTest;
+
+bool RrepHeaderTest::RunTests ()
+{
+  bool result(true);
+
+  RrepHeader h;
+  h.SetDst (Ipv4Address("1.2.3.4"));
+  h.SetDstSeqno (123);
+  h.SetSrc (Ipv4Address("4.3.2.1"));
+  h.SetLifeTime(12);
+  h.SetAckRequired(true);
+  NS_TEST_ASSERT(h.GetAckRequired ());
+  h.SetAckRequired(false);
+  NS_TEST_ASSERT(!h.GetAckRequired ());
+  h.SetPrefixSize(2);
+  uint8_t sz = h.GetPrefixSize();
+  NS_TEST_ASSERT_EQUAL(2, sz);
+
+  Ptr<Packet> p = Create<Packet> ();
+  p->AddHeader (h);
+  RrepHeader h2;
+  uint32_t bytes = p->RemoveHeader(h2);
+  NS_TEST_ASSERT_EQUAL (bytes, 20);
+  NS_TEST_ASSERT_EQUAL (h, h2);
+  return result;
+}
+#endif
+
+//-----------------------------------------------------------------------------
+// RREP-ACK
+//-----------------------------------------------------------------------------
+
+RrepAckHeader::RrepAckHeader () : reserved(0)
+{
+}
+
+TypeId
+RrepAckHeader::GetInstanceTypeId() const
+{
+  return TypeId();
+}
+
+uint32_t
+RrepAckHeader::GetSerializedSize () const
+{
+  return 2;
+}
+
+void
+RrepAckHeader::Serialize (Buffer::Iterator i) const
+{
+  i.WriteU8(type());
+  i.WriteU8(reserved);
+}
+
+uint32_t
+RrepAckHeader::Deserialize (Buffer::Iterator start)
+{
+  Buffer::Iterator i = start;
+  uint8_t t = i.ReadU8 ();
+  NS_ASSERT (t == type());
+  reserved = i.ReadU8 ();
+  uint32_t dist = i.GetDistanceFrom (start);
+  NS_ASSERT (dist == GetSerializedSize ());
+  return dist;
+}
+
+void
+RrepAckHeader::Print (std::ostream &os) const
+{
+  // TODO
+}
+
+
+bool
+RrepAckHeader::operator==(RrepAckHeader const & o) const
+{
+  return reserved == o.reserved;
+}
+
+std::ostream & operator<<(std::ostream & os, RrepAckHeader const & h)
+{
+  h.Print(os);
+  return os;
+}
+
+#ifdef RUN_SELF_TESTS
+/// Unit test for RREP-ACK
+struct RrepAckHeaderTest : public Test
+{
+  RrepAckHeaderTest () : Test ("AODV/RREP-ACK") {}
+  virtual bool RunTests();
+};
+
+/// Test instance
+static RrepAckHeaderTest g_RrepAckHeaderTest;
+
+bool RrepAckHeaderTest::RunTests ()
+{
+  bool result(true);
+
+  RrepAckHeader h;
+  Ptr<Packet> p = Create<Packet> ();
+  p->AddHeader (h);
+  RrepAckHeader h2;
+  uint32_t bytes = p->RemoveHeader(h2);
+  NS_TEST_ASSERT_EQUAL (bytes, 2);
+  NS_TEST_ASSERT_EQUAL (h, h2);
+  return result;
+}
+#endif
+
+//-----------------------------------------------------------------------------
+// RERR
+//-----------------------------------------------------------------------------
+RerrHeader::RerrHeader() : er_flag(0), reserved(0)
+{
+}
+
+TypeId
+RerrHeader::GetInstanceTypeId() const
+{
+  return TypeId();
+}
+
+uint32_t
+RerrHeader::GetSerializedSize () const
+{
+  return ( 4 + 8*GetDestCount());
+}
+
+void
+RerrHeader::Serialize (Buffer::Iterator i) const
+{
+  i.WriteU8(type());
+  i.WriteU8(er_flag);
+  i.WriteU8(reserved);
+  i.WriteU8(GetDestCount());
+  std::map<Ipv4Address, uint32_t>::const_iterator j;
+  for(j = unreachable_dst.begin(); j != unreachable_dst.end(); ++j)
+  {
+    WriteTo (i, (*j).first);
+    i.WriteHtonU32 ((*j).second);
+  }
+}
+
+uint32_t
+RerrHeader::Deserialize (Buffer::Iterator start)
+{
+  Buffer::Iterator i = start;
+  uint8_t t = i.ReadU8 ();
+  NS_ASSERT (t == type());
+
+  er_flag = i.ReadU8 ();
+  reserved = i.ReadU8 ();
+  uint8_t dest = i.ReadU8 ();
+  unreachable_dst.clear();
+  Ipv4Address address;
+  uint32_t seqNo;
+  for(uint8_t k = 0; k < dest; ++k)
+  {
+    ReadFrom (i, address);
+    seqNo = i.ReadNtohU32 ();
+    unreachable_dst.insert(std::make_pair(address, seqNo));
+  }
+
+  uint32_t dist = i.GetDistanceFrom (start);
+  NS_ASSERT (dist == GetSerializedSize ());
+  return dist;
+}
+
+void
+RerrHeader::Print (std::ostream &os) const
+{
+  // TODO
+}
+
+void
+RerrHeader::SetNoDelete(bool f)
+{
+  if (f) er_flag |= (1 << 0);
+  else   er_flag &= ~(1 << 0);
+}
+
+bool
+RerrHeader::GetNoDelete() const
+{
+  return (er_flag & (1 << 0));
+}
+
+bool
+RerrHeader::AddUnDestination(Ipv4Address dst, uint32_t seqNo)
+{
+  if(unreachable_dst.find(dst) != unreachable_dst.end())
+    return false;
+  
+  NS_ASSERT (GetDestCount() < 255); // can't support more than 255 destinations in single RERR
+  unreachable_dst.insert(std::make_pair(dst, seqNo));
+  return true;
+}
+
+bool
+RerrHeader::operator==(RerrHeader const & o) const
+{
+  if (er_flag != o.er_flag || reserved != o.reserved || GetDestCount() != o.GetDestCount())
+    return false;
+  
+  std::map<Ipv4Address, uint32_t>::const_iterator j = unreachable_dst.begin();
+  std::map<Ipv4Address, uint32_t>::const_iterator k = o.unreachable_dst.begin();
+  for(uint8_t i = 0; i < GetDestCount(); ++i)
+  {
+    if ((j->first != k->first ) || (j->second != k->second))
+      return false;
+    
+    j++;
+    k++;
+  }
+  return true;
+}
+
+std::ostream & operator<<(std::ostream & os, RerrHeader const & h)
+{
+  h.Print(os);
+  return os;
+}
+
+#ifdef RUN_SELF_TESTS
+/// Unit test for RERR
+struct RerrHeaderTest : public Test
+{
+  RerrHeaderTest () : Test ("AODV/RERR") {}
+  virtual bool RunTests();
+};
+
+/// Test instance
+static RerrHeaderTest g_RerrHeaderTest;
+
+bool RerrHeaderTest::RunTests ()
+{
+  bool result(true);
+
+  RerrHeader h;
+  h.SetNoDelete(true);
+  NS_TEST_ASSERT(h.GetNoDelete());
+  Ipv4Address dst = Ipv4Address("1.2.3.4");
+  NS_TEST_ASSERT(h.AddUnDestination(dst, 12));
+  NS_TEST_ASSERT_EQUAL(h.GetDestCount(),1);
+  NS_TEST_ASSERT(!h.AddUnDestination(dst, 13));
+  Ipv4Address dst2 = Ipv4Address("4.3.2.1");
+  NS_TEST_ASSERT(h.AddUnDestination(dst2, 12));
+  NS_TEST_ASSERT_EQUAL(h.GetDestCount(), 2);
+
+  Ptr<Packet> p = Create<Packet> ();
+  p->AddHeader (h);
+  RerrHeader h2;
+  uint32_t bytes = p->RemoveHeader(h2);
+  NS_TEST_ASSERT_EQUAL (bytes, h.GetSerializedSize());
+  NS_TEST_ASSERT_EQUAL (h, h2);
+  return result;
+}
+#endif
+}}
---
+++ src/routing/aodv/aodv-packet.h
@@ -0,0 +1,287 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 1997, 1998 Carnegie Mellon University.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Authors: The AODV code developed by the CMU/MONARCH group was optimized and
+ * tuned by Samir Das and Mahesh Marina, University of Cincinnati.
+ * The work was partially done in Sun Microsystems.
+ *
+ * Ported to ns-3 by Elena Borovkova <borovkovaes@iitp.ru>
+ */
+#ifndef AODVPACKET_H_
+#define AODVPACKET_H_
+
+#include <stdint.h>
+#include <iostream>
+#include "ns3/header.h"
+#include "ns3/ipv4-address.h"
+#include <map>
+
+// TODO avoid defines, use protocol attributes
+#define HELLO_INTERVAL          1               // 1000 ms
+#define ALLOWED_HELLO_LOSS      3               // packets
+
+namespace ns3 {
+namespace aodv {
+/** 
+ * \ingroup aodv
+ * \brief AODV control types
+ */
+enum MessageType 
+{
+  AODVTYPE_RREQ  = 0x02,  //!< AODVTYPE_RREQ
+  AODVTYPE_RREP  = 0x04,  //!< AODVTYPE_RREP
+  AODVTYPE_RERR  = 0x08,  //!< AODVTYPE_RERR
+  AODVTYPE_RREP_ACK = 0x10//!< AODVTYPE_RREP_ACK
+};
+
+/**
+* \ingroup aodv
+* \brief   Route Request (RREQ) Message Format
+  \verbatim
+  0                   1                   2                   3
+  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |     Type      |J|R|G|D|U|   Reserved          |   Hop Count   |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                            RREQ ID                            |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                    Destination IP Address                     |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                  Destination Sequence Number                  |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                    Originator IP Address                      |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                  Originator Sequence Number                   |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+*/
+class RreqHeader : public Header 
+{
+public:
+  RreqHeader ();
+
+  ///\name Header serialization/deserialization
+  //\{
+  TypeId GetInstanceTypeId() const;
+  uint32_t GetSerializedSize () const;
+  void Serialize (Buffer::Iterator start) const;
+  uint32_t Deserialize (Buffer::Iterator start);
+  void Print (std::ostream &os) const;
+  //\}
+
+  ///\name Fields
+  //\{
+  void SetHopCount (uint8_t count) { rq_hop_count = count; }
+  uint8_t GetHopCount () const { return rq_hop_count; }
+  void SetId (uint32_t id) { rq_bcast_id = id; }
+  uint8_t GetId () const { return rq_bcast_id; }  
+  void SetDst (Ipv4Address a) { rq_dst = a; }
+  Ipv4Address GetDst () const { return rq_dst; }
+  void SetDstSeqno (uint32_t s) { rq_dst_seqno = s; }
+  uint32_t GetDstSeqno () const { return rq_dst_seqno; }
+  void SetSrc (Ipv4Address a) { rq_src = a; }
+  Ipv4Address GetSrc () const { return rq_src; }  
+  void SetSrcSeqno (uint32_t s) { rq_src_seqno = s; }
+  uint32_t GetSrcSeqno () const { return rq_src_seqno; }
+  //\}
+
+  ///\name Flags
+  //\{
+  void SetGratiousRrep (bool f);
+  bool GetGratiousRrep () const;
+  void SetDestinationOnly (bool f);
+  bool GetDestinationOnly () const;
+  void SetUnknownSeqno (bool f);
+  bool GetUnknownSeqno () const;
+  //\}
+
+  bool operator==(RreqHeader const & o) const;
+private:
+  static MessageType type() { return AODVTYPE_RREQ; }
+
+  uint8_t        rq_flags;       ///< |J|R|G|D|U| bit flags, see RFC
+  uint8_t        reserved;       ///< Not used
+  uint8_t        rq_hop_count;   ///< Hop Count
+  uint32_t       rq_bcast_id;    ///< RREQ ID
+  Ipv4Address    rq_dst;         ///< Destination IP Address
+  uint32_t       rq_dst_seqno;   ///< Destination Sequence Number
+  Ipv4Address    rq_src;         ///< Source IP Address
+  uint32_t       rq_src_seqno;   ///< Source Sequence Number
+};
+
+std::ostream & operator<<(std::ostream & os, RreqHeader const &);
+
+/**
+* \ingroup aodv
+* \brief Route Reply (RREP) Message Format
+  \verbatim
+  0                   1                   2                   3
+  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |     Type      |R|A|    Reserved     |Prefix Sz|   Hop Count   |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                     Destination IP address                    |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                  Destination Sequence Number                  |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                    Originator IP address                      |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |                           Lifetime                            |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+*/
+class RrepHeader : public Header
+{
+public:
+  RrepHeader();
+  ///\name Header serialization/deserialization
+  //\{
+  TypeId GetInstanceTypeId() const;
+  uint32_t GetSerializedSize () const;
+  void Serialize (Buffer::Iterator start) const;
+  uint32_t Deserialize (Buffer::Iterator start);
+  void Print (std::ostream &os) const;
+  //\}
+
+  ///\name Fields
+  //\{
+  void SetHopCount (uint8_t count) { rp_hop_count = count; }
+  uint8_t GetHopCount () const { return rp_hop_count; }
+  void SetDst (Ipv4Address a) { rp_dst = a; }
+  Ipv4Address GetDst () const { return rp_dst; }
+  void SetDstSeqno (uint32_t s) { rp_dst_seqno = s; }
+  uint32_t GetDstSeqno () const { return rp_dst_seqno; }
+  void SetSrc (Ipv4Address a) { rp_src = a; }
+  Ipv4Address GetSrc () const { return rp_src; }
+  void SetLifeTime (uint32_t t) { rp_lifetime = t; }
+  uint32_t GetLifeTime () const { return rp_lifetime; }
+  //\}
+
+  ///\name Flags
+  //\{
+  void SetAckRequired (bool f);
+  bool GetAckRequired () const;
+  void SetPrefixSize(uint8_t sz);
+  uint8_t GetPrefixSize() const;
+  //\}
+
+  /// Configure RREP to be a Hello message
+  void SetHello(Ipv4Address src, uint32_t srcSeqNo);
+  
+  bool operator==(RrepHeader const & o) const;
+private:
+  static MessageType type() { return AODVTYPE_RREP; }
+  uint8_t       rp_flags;	    ///< A - acknowledgment required flag
+  uint8_t       prefixSize;	    ///< Prefix Size
+  uint8_t	rp_hop_count;       ///< Hop Count
+  Ipv4Address   rp_dst;             ///< Destination IP Address
+  uint32_t      rp_dst_seqno;       ///< Destination Sequence Number
+  Ipv4Address	rp_src;             ///< Source IP Address
+  uint32_t      rp_lifetime;        ///< Lifetime
+};
+
+std::ostream & operator<<(std::ostream & os, RrepHeader const &);
+
+/**
+* \ingroup aodv
+* \brief Route Reply Acknowledgment (RREP-ACK) Message Format
+  \verbatim  
+  0                   1
+  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |     Type      |   Reserved    |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+*/
+class RrepAckHeader : public Header
+{
+public:
+  RrepAckHeader ();
+
+  ///\name Header serialization/deserialization
+  //\{
+  TypeId GetInstanceTypeId() const;
+  uint32_t GetSerializedSize () const;
+  void Serialize (Buffer::Iterator start) const;
+  uint32_t Deserialize (Buffer::Iterator start);
+  void Print (std::ostream &os) const;
+  //\}
+
+  bool operator==(RrepAckHeader const & o) const;
+private:
+  static MessageType type() { return AODVTYPE_RREP_ACK; }
+  uint8_t       reserved;
+};
+std::ostream & operator<<(std::ostream & os, RrepAckHeader const &);
+
+
+/**
+* \ingroup aodv
+* \brief Route Error (RERR) Message Format
+  \verbatim
+  0                   1                   2                   3
+  0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1 2 3 4 5 6 7 8 9 0 1
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |     Type      |N|          Reserved           |   DestCount   |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |            Unreachable Destination IP Address (1)             |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |         Unreachable Destination Sequence Number (1)           |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-|
+  |  Additional Unreachable Destination IP Addresses (if needed)  |
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  |Additional Unreachable Destination Sequence Numbers (if needed)|
+  +-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+-+
+  \endverbatim
+*/
+class RerrHeader : public Header
+{
+public:
+  RerrHeader ();
+
+  ///\name Header serialization/deserialization
+  //\{
+  TypeId GetInstanceTypeId() const;
+  uint32_t GetSerializedSize () const;
+  void Serialize (Buffer::Iterator i) const;
+  uint32_t Deserialize (Buffer::Iterator start);
+  void Print (std::ostream &os) const;
+  //\}
+
+  ///\name No delete flag
+  //\{
+  void SetNoDelete(bool f);
+  bool GetNoDelete() const;
+  //\}
+
+  bool AddUnDestination(Ipv4Address dst, uint32_t seqNo);
+  uint8_t GetDestCount() const { return (uint8_t)unreachable_dst.size(); }
+  bool operator==(RerrHeader const & o) const;
+private:
+  static MessageType type() { return AODVTYPE_RERR; }
+  
+  uint8_t er_flag;        ///< No delete flag
+  uint8_t reserved;       ///< Not used
+  
+  /// List of Unreachable destination IP addresses and sequence numbers
+  std::map<Ipv4Address, uint32_t> unreachable_dst;
+};
+
+std::ostream & operator<<(std::ostream & os, RerrHeader const &);
+}
+}
+#endif /* AODVPACKET_H_ */
