993b2867dc7cf035d251f406231337f037dba523
Nicola Baldo <nbaldo@cttc.es>
2011-03-29 17:33:07 +0200

    removed useless getters

---
+++ src/lte/helper/lena-helper.cc
@@ -0,0 +1,303 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2011 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Nicola Baldo <nbaldo@cttc.es> (re-wrote from scratch this helper)
+ *         Giuseppe Piro <g.piro@poliba.it> (parts of the PHY & channel  creation & configuration copied from the GSoC 2011 code)
+ */
+
+
+#include "lena-helper.h"
+
+#include <ns3/lte-enb-rrc.h>
+#include <ns3/lte-ue-rrc.h>
+#include <ns3/lte-ue-mac.h>
+#include <ns3/lte-enb-mac.h>
+
+#include <ns3/lte-enb-phy.h>
+#include <ns3/lte-ue-phy.h>
+#include <ns3/lte-spectrum-phy.h>
+#include <ns3/lte-sinr-chunk-processor.h>
+#include <ns3/single-model-spectrum-channel.h>
+#include <ns3/friis-spectrum-propagation-loss.h>
+
+#include <ns3/lte-enb-net-device.h>
+#include <ns3/lte-ue-net-device.h>
+
+#include <ns3/lte-spectrum-value-helper.h>
+#include <ns3/lte-enb-mac.h>
+#include <ns3/pf-ff-mac-scheduler.h>
+#include <ns3/lte-enb-rrc.h>
+#include <ns3/lte-ue-mac.h>
+#include <ns3/lte-ue-rrc.h>
+
+
+namespace ns3 {
+
+
+LenaHelper::LenaHelper (void)
+  : m_downlinkChannel (CreateObject<SingleModelSpectrumChannel> ()),
+    m_uplinkChannel (CreateObject<SingleModelSpectrumChannel> ())
+{
+  Ptr<SpectrumPropagationLossModel> model = CreateObject<FriisSpectrumPropagationLossModel> ();
+  m_downlinkChannel->AddSpectrumPropagationLossModel (model);
+  
+  SetScheduler ("RrFfMacScheduler"); // default scheduler
+}
+
+LenaHelper::~LenaHelper (void)
+{
+  m_downlinkChannel = 0;
+  m_uplinkChannel = 0;
+}
+
+
+
+
+void 
+LenaHelper::SetScheduler (std::string type,
+                          std::string n0, const AttributeValue &v0,
+                          std::string n1, const AttributeValue &v1,
+                          std::string n2, const AttributeValue &v2,
+                          std::string n3, const AttributeValue &v3,
+                          std::string n4, const AttributeValue &v4,
+                          std::string n5, const AttributeValue &v5,
+                          std::string n6, const AttributeValue &v6,
+                          std::string n7, const AttributeValue &v7)
+{
+  m_scheduler = ObjectFactory ();
+  m_scheduler.SetTypeId (type);
+  m_scheduler.Set (n0, v0);
+  m_scheduler.Set (n1, v1);
+  m_scheduler.Set (n2, v2);
+  m_scheduler.Set (n3, v3);
+  m_scheduler.Set (n4, v4);
+  m_scheduler.Set (n5, v5);
+  m_scheduler.Set (n6, v6);
+  m_scheduler.Set (n7, v7);
+}
+
+
+
+NetDeviceContainer
+LenaHelper::InstallEnbDevice (NodeContainer c)
+{
+  NetDeviceContainer devices;
+  for (NodeContainer::Iterator i = c.Begin (); i != c.End (); ++i)
+    {
+      Ptr<Node> node = *i;
+      Ptr<NetDevice> device = InstallSingleEnbDevice (node);
+      devices.Add (device);
+    }
+  return devices;
+}
+
+NetDeviceContainer
+LenaHelper::InstallUeDevice (NodeContainer c)
+{
+  NetDeviceContainer devices;
+  for (NodeContainer::Iterator i = c.Begin (); i != c.End (); ++i)
+    {
+      Ptr<Node> node = *i;
+      Ptr<NetDevice> device = InstallSingleUeDevice (node);
+      devices.Add (device);
+    }
+  return devices;
+}
+
+
+void
+LenaHelper::SetEnbDeviceAttribute (std::string name, const AttributeValue &value)
+{
+
+}
+
+void
+LenaHelper::SetUeDeviceAttribute (std::string name, const AttributeValue &value)
+{
+
+}
+
+
+Ptr<NetDevice>
+LenaHelper::InstallSingleEnbDevice (Ptr<Node> n)
+{
+  Ptr<LteEnbPhy> phy = CreateObject<LteEnbPhy> ();
+
+  Ptr<LteSpectrumPhy> dlPhy = CreateObject<LteSpectrumPhy> ();
+  Ptr<LteSpectrumPhy> ulPhy = CreateObject<LteSpectrumPhy> ();
+
+  phy->SetDownlinkSpectrumPhy (dlPhy);
+  phy->SetUplinkSpectrumPhy (ulPhy);
+
+  Ptr<SpectrumValue> noisePsd = LteSpectrumValueHelper::CreateUplinkNoisePowerSpectralDensity ();
+  ulPhy->SetNoisePowerSpectralDensity (noisePsd);
+
+  dlPhy->SetChannel (m_downlinkChannel);
+  ulPhy->SetChannel (m_uplinkChannel);
+
+  Ptr<MobilityModel> mm = n->GetObject<MobilityModel> ();
+  NS_ASSERT_MSG (mm, "MobilityModel needs to be set on node before calling LenaHelper::InstallUeDevice ()");
+  dlPhy->SetMobility (mm);
+  ulPhy->SetMobility (mm);
+
+  m_uplinkChannel->AddRx (ulPhy);
+  Ptr<LteEnbMac> mac = CreateObject<LteEnbMac> ();
+  Ptr<FfMacScheduler> sched = m_scheduler.Create<FfMacScheduler> ();
+  //Ptr<FfMacScheduler> sched = Create<PfFfMacScheduler> ();
+  Ptr<LteEnbRrc> rrc = Create<LteEnbRrc> ();
+  Ptr<LteEnbNetDevice> dev = CreateObject<LteEnbNetDevice> (n, phy, mac, sched, rrc);
+  phy->SetDevice (dev);
+  dlPhy->SetDevice (dev);
+  ulPhy->SetDevice (dev);
+
+  n->AddDevice (dev);
+  ulPhy->SetPhyMacRxEndOkCallback (MakeCallback (&LteEnbPhy::PhyPduReceived, phy));
+
+  dev->Start ();
+  return dev;
+}
+
+Ptr<NetDevice>
+LenaHelper::InstallSingleUeDevice (Ptr<Node> n)
+{
+  Ptr<LteUePhy> phy = CreateObject<LteUePhy> ();
+
+  Ptr<LteSpectrumPhy> dlPhy = CreateObject<LteSpectrumPhy> ();
+  Ptr<LteSpectrumPhy> ulPhy = CreateObject<LteSpectrumPhy> ();
+
+  phy->SetDownlinkSpectrumPhy (dlPhy);
+  phy->SetUplinkSpectrumPhy (ulPhy);
+
+  Ptr<SpectrumValue> noisePsd = LteSpectrumValueHelper::CreateDownlinkNoisePowerSpectralDensity ();
+  dlPhy->SetNoisePowerSpectralDensity (noisePsd);
+
+  Ptr<LteCqiSinrChunkProcessor> p = Create<LteCqiSinrChunkProcessor> (phy->GetObject<LtePhy> ());
+  dlPhy->AddSinrChunkProcessor (p);
+
+  dlPhy->SetChannel (m_downlinkChannel);
+  ulPhy->SetChannel (m_uplinkChannel);
+
+  Ptr<MobilityModel> mm = n->GetObject<MobilityModel> ();
+  NS_ASSERT_MSG (mm, "MobilityModel needs to be set on node before calling LenaHelper::InstallUeDevice ()");
+  dlPhy->SetMobility (mm);
+  ulPhy->SetMobility (mm);
+
+  m_downlinkChannel->AddRx (dlPhy);
+  
+  Ptr<LteUeMac> mac = CreateObject<LteUeMac> ();
+  Ptr<LteUeRrc> rrc = Create<LteUeRrc> ();
+  Ptr<LteUeNetDevice> dev = CreateObject<LteUeNetDevice> (n, phy, mac, rrc);
+  phy->SetDevice (dev);
+  dlPhy->SetDevice (dev);
+  ulPhy->SetDevice (dev);
+
+  n->AddDevice (dev);
+  dlPhy->SetPhyMacRxEndOkCallback (MakeCallback (&LteUePhy::PhyPduReceived, phy));
+
+  dev->Start ();
+  return dev;
+}
+
+
+void
+LenaHelper::Attach (NetDeviceContainer ueDevices, Ptr<NetDevice> enbDevice)
+{
+  for (NetDeviceContainer::Iterator i = ueDevices.Begin (); i != ueDevices.End (); ++i)
+    {
+      Attach (*i, enbDevice);
+    }
+}
+
+void
+LenaHelper::Attach (Ptr<NetDevice> ueDevice, Ptr<NetDevice> enbDevice)
+{
+  // setup RRC connection
+  Ptr<LteEnbRrc> enbRrc = enbDevice->GetObject<LteEnbNetDevice> ()->GetRrc ();
+  uint16_t rnti = enbRrc->AddUe ();
+  Ptr<LteUeRrc> ueRrc = ueDevice->GetObject<LteUeNetDevice> ()->GetRrc ();
+  ueRrc->ConfigureUe (rnti);
+
+  // attach UE PHY to eNB
+  ueDevice->GetObject<LteUeNetDevice> ()->SetTargetEnb (enbDevice->GetObject<LteEnbNetDevice> ());
+
+  // WILD HACK - should be done through PHY SAP, probably passing by RRC
+  Ptr<LteUePhy> uePhy = ueDevice->GetObject<LteUeNetDevice> ()->GetPhy ();
+  uePhy->SetRnti (rnti);
+
+  Ptr<LteEnbPhy> enbPhy = enbDevice->GetObject<LteEnbNetDevice> ()->GetPhy ();
+  enbPhy->AddUePhy (rnti, uePhy);
+}
+
+
+void
+LenaHelper::ActivateEpsBearer (NetDeviceContainer ueDevices, EpsBearer bearer)
+{
+  for (NetDeviceContainer::Iterator i = ueDevices.Begin (); i != ueDevices.End (); ++i)
+    {
+      ActivateEpsBearer (*i, bearer);
+    }
+}
+
+
+void
+LenaHelper::ActivateEpsBearer (Ptr<NetDevice> ueDevice, EpsBearer bearer)
+{
+  Ptr<LteEnbNetDevice> enbDevice = ueDevice->GetObject<LteUeNetDevice> ()->GetTargetEnb ();
+  Ptr<LteEnbRrc> enbRrc = enbDevice->GetObject<LteEnbNetDevice> ()->GetRrc ();
+  Ptr<LteUeRrc> ueRrc = ueDevice->GetObject<LteUeNetDevice> ()->GetRrc ();
+  uint16_t rnti = ueRrc->GetRnti ();
+  uint8_t lcid = enbRrc->SetupRadioBearer (rnti, bearer);
+  ueRrc->SetupRadioBearer (rnti, bearer, lcid);
+}
+
+
+void
+LenaHelper::EnableLogComponents (void)
+{
+  LogComponentEnable ("LteEnbRrc", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteUeRrc", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteEnbMac", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteUeMac", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteRlc", LOG_LEVEL_ALL);
+  LogComponentEnable ("RrFfMacScheduler", LOG_LEVEL_ALL);
+  LogComponentEnable ("PfFfMacScheduler", LOG_LEVEL_ALL);
+
+  LogComponentEnable ("LtePhy", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteEnbPhy", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteUePhy", LOG_LEVEL_ALL);
+
+  LogComponentEnable ("LteSpectrumPhy", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteInterference", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteSinrChunkProcessor", LOG_LEVEL_ALL);
+ 
+  LogComponentEnable ("LtePropagationLossModel", LOG_LEVEL_ALL);
+  LogComponentEnable ("LossModel", LOG_LEVEL_ALL);
+  LogComponentEnable ("ShadowingLossModel", LOG_LEVEL_ALL);
+  LogComponentEnable ("PenetrationLossModel", LOG_LEVEL_ALL);
+  LogComponentEnable ("MultipathLossModel", LOG_LEVEL_ALL);
+  LogComponentEnable ("PathLossModel", LOG_LEVEL_ALL);
+
+  LogComponentEnable ("LteNetDevice", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteUeNetDevice", LOG_LEVEL_ALL);
+  LogComponentEnable ("LteEnbNetDevice", LOG_LEVEL_ALL);
+
+  LogComponentEnable ("RlcStatsCalculator", LOG_LEVEL_ALL);
+}
+
+
+
+} // namespace ns3
---
+++ src/lte/model/lte-enb-net-device.cc
@@ -0,0 +1,227 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+/*
+ * Copyright (c) 2010 TELEMATICS LAB, DEE - Politecnico di Bari
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation;
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *
+ * Author: Giuseppe Piro  <g.piro@poliba.it>
+ * Author: Marco Miozzo <mmiozzo@cttc.es> : Update to FF API Architecture
+ * Author: Nicola Baldo <nbaldo@cttc.es>  : Integrated with new RRC and MAC architecture
+ */
+
+#include <ns3/llc-snap-header.h>
+#include <ns3/simulator.h>
+#include <ns3/callback.h>
+#include <ns3/node.h>
+#include <ns3/packet.h>
+#include <ns3/lte-net-device.h>
+#include <ns3/packet-burst.h>
+#include <ns3/uinteger.h>
+#include <ns3/trace-source-accessor.h>
+#include <ns3/pointer.h>
+#include <ns3/enum.h>
+#include <ns3/lte-amc.h>
+#include <ns3/lte-enb-mac.h>
+#include <ns3/lte-enb-net-device.h>
+#include <ns3/lte-enb-rrc.h>
+#include <ns3/lte-ue-net-device.h>
+#include <ns3/lte-enb-phy.h>
+#include <ns3/ff-mac-scheduler.h>
+//#include <ns3/rr-ff-mac-scheduler.h>
+#include <ns3/pf-ff-mac-scheduler.h>
+
+
+NS_LOG_COMPONENT_DEFINE ("LteEnbNetDevice");
+
+namespace ns3 {
+
+NS_OBJECT_ENSURE_REGISTERED ( LteEnbNetDevice);
+
+uint16_t LteEnbNetDevice::m_cellIdCounter = 0;
+
+TypeId LteEnbNetDevice::GetTypeId (void)
+{
+  static TypeId
+  tid =
+    TypeId ("ns3::LteEnbNetDevice")
+    .SetParent<LteNetDevice> ()
+    .AddConstructor<LteEnbNetDevice> ()
+    .AddAttribute ("LteEnbRrc",
+                   "The RRC associated to this EnbNetDevice.",
+                   PointerValue (),
+                   MakePointerAccessor (&LteEnbNetDevice::m_rrc),
+                   MakePointerChecker <LteEnbRrc> ())
+    ;
+  return tid;
+}
+
+LteEnbNetDevice::LteEnbNetDevice (void)
+{
+  NS_LOG_FUNCTION (this);
+  NS_FATAL_ERROR ("This constructor should not be called");
+  InitLteEnbNetDevice ();
+}
+
+LteEnbNetDevice::LteEnbNetDevice (Ptr<Node> node, Ptr<LteEnbPhy> phy, Ptr<LteEnbMac> mac, Ptr<FfMacScheduler> sched, Ptr<LteEnbRrc> rrc)
+{
+  NS_LOG_FUNCTION (this);
+  m_phy = phy;
+  m_mac = mac;
+  m_scheduler = sched;
+  m_rrc = rrc;
+  InitLteEnbNetDevice ();
+  SetNode (node);
+}
+
+LteEnbNetDevice::~LteEnbNetDevice (void)
+{
+  NS_LOG_FUNCTION (this);
+}
+
+void
+LteEnbNetDevice::DoDispose ()
+{
+  NS_LOG_FUNCTION (this);
+
+//   m_mac->Dispose ();
+//   m_mac = 0;
+// 
+//   m_rrc->Dispose ();
+//   m_rrc = 0;
+// 
+//   m_phy->Dispose ();
+//   m_phy = 0;
+
+  LteNetDevice::DoDispose ();
+}
+
+
+void
+LteEnbNetDevice::InitLteEnbNetDevice (void)
+{
+  NS_LOG_FUNCTION (this);
+
+  SetNode (0);
+  if (GetPhy () == 0)
+    {
+      NS_LOG_DEBUG (this << "PHY NULL");
+    }
+  else
+    {
+      NS_LOG_DEBUG (this << "PHY ! NULL");
+    }
+  
+  m_rrc->SetLteEnbCmacSapProvider (m_mac->GetLteEnbCmacSapProvider ());
+  m_mac->SetLteEnbCmacSapUser (m_rrc->GetLteEnbCmacSapUser ());
+  m_rrc->SetLteMacSapProvider (m_mac->GetLteMacSapProvider ());
+
+  m_mac->SetFfMacSchedSapProvider (m_scheduler->GetFfMacSchedSapProvider ());
+  m_mac->SetFfMacCschedSapProvider (m_scheduler->GetFfMacCschedSapProvider ());
+
+  m_scheduler->SetFfMacSchedSapUser (m_mac->GetFfMacSchedSapUser ());
+  m_scheduler->SetFfMacCschedSapUser (m_mac->GetFfMacCschedSapUser ());
+
+  GetPhy ()->GetObject<LteEnbPhy> ()->SetLteEnbPhySapUser (m_mac->GetLteEnbPhySapUser ());
+  m_mac->SetLteEnbPhySapProvider (GetPhy ()->GetObject<LteEnbPhy> ()->GetLteEnbPhySapProvider ());
+
+  m_rrc->ConfigureCell (25, 25);
+  NS_ASSERT_MSG (m_cellIdCounter < 65535, "max num eNBs exceeded");
+  m_cellId = ++m_cellIdCounter;
+
+  // WILD HACK -  should use the PHY SAP instead. Probably should handle this through the RRC
+  GetPhy ()->GetObject<LteEnbPhy> ()->DoSetBandwidth (25,25);
+  GetPhy ()->GetObject<LteEnbPhy> ()->DoSetCellId (m_cellId);
+  
+  Simulator::ScheduleNow (&LteEnbPhy::StartFrame, GetPhy ()->GetObject<LteEnbPhy> ());
+}
+
+
+Ptr<LteEnbMac>
+LteEnbNetDevice::GetMac (void)
+{
+  NS_LOG_FUNCTION (this);
+  return m_mac;
+}
+
+
+Ptr<LteEnbPhy>
+LteEnbNetDevice::GetPhy (void) const
+{
+  NS_LOG_FUNCTION (this);
+  return m_phy;
+}
+
+
+bool
+LteEnbNetDevice::DoSend (Ptr<Packet> packet, const Mac48Address& source,
+                      const Mac48Address& dest, uint16_t protocolNumber)
+{
+  NS_LOG_FUNCTION (this << source << dest << protocolNumber);
+
+  NS_FATAL_ERROR ("IP connectivity not implemented yet");
+
+  /*
+   * The classification of traffic in DL is done by the PGW (not
+   * by the eNB).
+   * Hovever, the core network is not implemented yet.
+   * For now the classification is managed by the eNB.
+   */
+
+  // if (protocolNumber == 2048)
+  //   {
+  //     // it is an IP packet
+  //   }
+
+  // if (protocolNumber != 2048 || bearer == 0)
+  //   {
+
+  //   }
+
+
+  return true;
+}
+
+
+void
+LteEnbNetDevice::DoReceive (Ptr<Packet> p)
+{
+  NS_LOG_FUNCTION (this << p);
+  ForwardUp (p->Copy ());
+}
+
+
+void
+LteEnbNetDevice::SendIdealPdcchMessage (void)
+{
+  NS_LOG_FUNCTION (this);
+  /*
+   * Get both PDCCH ideal message for UL and DL and
+   * set assigned resources to UEs using
+   * SendAssignedDLResources and SendAssignedULResources
+   */
+}
+
+Ptr<LteEnbRrc>
+LteEnbNetDevice::GetRrc ()
+{
+  return m_rrc;
+}
+
+uint16_t
+LteEnbNetDevice::GetCellId ()
+{
+  return m_cellId;
+}
+
+} // namespace ns3
